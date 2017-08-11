e44f8b4e020f8cadf63a2de377c9c0935e67575a
marcio antonio <marcinho1926@outlook.com>
2017-08-10 21:33:20 -0300

    Controllers

---
+++ SistemaComercial/Controllers/FinanceiroController.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaComercial.Controllers
+{
+    public class FinanceiroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ContasAPagar()
+        {
+            return View("Index");
+        }
+        public IActionResult ContasAReceber()
+        {
+            return View("Index");
+        }
+    }
+}
---
+++ SistemaComercial/Controllers/ProdutoController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SistemaComercial.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Estoque()
+        {
+            return View("Index");
+        }
+    }
+}
